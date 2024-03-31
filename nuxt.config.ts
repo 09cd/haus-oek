79cb63e2e989bffbc6cd7a494c311935c5685e8a
Chen Yang Diep <diepchenyang@icloud.com>
2024-03-31 19:02:28 +0200

    Add Font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    css: ["~/assets/scss/main.scss"],
+  devtools: { enabled: true },
+  css: ["~/assets/scss/main.scss"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://use.typekit.net/ydx3bvs.css",
+        },
+      ],
+    },
+  },
 });
