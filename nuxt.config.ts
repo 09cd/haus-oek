1c84855917a8c6469b97f4a5de2e7c87237ac5c7
Chen Yang Diep <diepchenyang@icloud.com>
2024-04-01 23:17:04 +0200

    Add SCSS Variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ["~/assets/scss/main.scss"],
-  app: {
-    head: {
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://use.typekit.net/ydx3bvs.css",
+    devtools: { enabled: true },
+    css: ["~/assets/scss/main.scss"],
+    app: {
+        head: {
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://use.typekit.net/ydx3bvs.css",
+                },
+            ],
         },
-      ],
     },
-  },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData:
+                        '@use "~/assets/scss/_variables.scss" as *;',
+                },
+            },
+        },
+    },
 });
