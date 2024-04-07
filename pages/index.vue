f97b34db6a97aa68beb737c91f13d80780573cd4
Chen Yang Diep <diepchenyang@icloud.com>
2024-04-07 21:56:12 +0200

    Home Page Hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <script></script>
 
 <template>
-    <h1>Home</h1>
+    <div class="home-container">
+        <div class="general-info">
+            <div class="time">10:56 AM CET</div>
+            <div class="location">51.2230° N, 6.7825° E</div>
+        </div>
+        <h1>
+            Welcome to my Blog <br />
+            This is my repository of all knowledge I gain
+        </h1>
+    </div>
+    <div class="latest-articles"></div>
 </template>
 
-<style></style>
+<style lang="scss">
+.home-container {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 2rem;
+    padding-left: calc(2rem + 40%);
+}
+</style>
