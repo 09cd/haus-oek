f34909b157162369c709e5a2048cc49c033a4c18
Chen Yang Diep <diepchenyang@icloud.com>
2024-04-08 14:20:33 +0200

    Add Footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script></script>
+
+<template>
+    <footer class="footer">
+        <div>Imprint, Privacy Policy</div>
+        <div class="copyright">Copyright © 2024 Code With Sway</div>
+    </footer>
+</template>
+
+<style>
+.footer {
+    display: flex;
+    padding: 0 2rem 2rem;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+
+    .copyright {
+        position: absolute;
+        left: calc(2rem + 40%);
+    }
+}
+</style>
