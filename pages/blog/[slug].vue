ebdc34ea22af76553daaf674aa8f1dcfb2c0cf76
Chen Yang Diep <diepchenyang@icloud.com>
2024-04-19 11:31:47 +0200

    Nuxt Link for Anchor Links

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -9,16 +9,25 @@ const { data } = await useAsyncData(`content-${route.path}`, () =>
 <template>
     <main>
         <nav>
-            <div>01 Introduction</div>
-            <div>02 The Independent Spirit</div>
-            <div>03 Curious Explorer</div>
-            <div>04 Purrfect Nap</div>
+            <NuxtLink :to="{ path: route.path, hash: '#introduction' }"
+                >01 Introduction</NuxtLink
+            >
+            <NuxtLink
+                :to="{ path: route.path, hash: '#the-independent-spirit' }"
+                >02 The Independent Spirit</NuxtLink
+            >
+            <NuxtLink :to="{ path: route.path, hash: '#curious-explorer' }"
+                >03 Curious Explorer</NuxtLink
+            >
+            <NuxtLink :to="{ path: route.path, hash: '#purrfect-nap' }"
+                >04 Purrfect Nap</NuxtLink
+            >
         </nav>
         <ContentRenderer :value="data" />
     </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 main {
     padding: 4rem 2rem 6rem;
     padding-left: calc(2rem + 40%);
@@ -31,21 +40,36 @@ main {
         display: flex;
         flex-direction: column;
         gap: 0.75rem;
+
+        a {
+            color: $black;
+            text-decoration: none;
+        }
     }
 
     :deep(p) {
-        font-size: 1.125rem;
+        /* font-size: 1.125rem; */
+        font-size: 24px;
         padding-bottom: 1rem;
         text-align: justify;
     }
 
     :deep(h1) {
-        font-size: 2rem;
+        /* font-size: 2rem; */
+        font-size: 48px;
     }
 
     :deep(h2) {
-        font-size: 1.25rem;
+        /* font-size: 1.25rem; */
+        font-size: 24px;
         padding: 4rem 0 1.5rem;
+    }
+
+    :deep(h2, h3, h4) {
+        a {
+            color: $black;
+            text-decoration: none;
+        }
     }
 
     :deep(img) {
