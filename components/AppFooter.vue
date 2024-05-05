973bfa98f7fbdd4e220e2f4f92391a7e04625c59
Chen Yang Diep <diepchenyang@icloud.com>
2024-05-05 16:09:04 +0200

    Adjust App Footer for Mobile

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,25 +4,48 @@
     <footer class="footer">
         <div>Imprint, Privacy Policy</div>
         <div class="copyright">Copyright © 2024 Code With Sway</div>
+        <!-- <div class="copyright-short">© 2024 Code With Sway</div> -->
     </footer>
-    <!-- <AppFooter /> -->
 </template>
 
 <style lang="scss" scoped>
 .footer {
     display: flex;
+    justify-content: space-between;
+    flex-direction: column;
     padding: 0 2rem 2rem;
     position: absolute;
     bottom: 0;
     left: 0;
     width: 100%;
 
+    @media (min-width: $bp-sm) {
+        flex-direction: row;
+    }
+
     .copyright {
-        position: absolute;
-        left: $pad-left-lg;
+        position: static;
+        display: none;
+
+        @media (min-width: $bp-sm) {
+            display: block;
+        }
+
+        @media (min-width: $bp-lg) {
+            position: absolute;
+            left: $pad-left-lg;
+        }
 
         @media (min-width: $bp-xl) {
             left: $pad-left-xl;
+        }
+    }
+
+    .copyright-short {
+        display: block;
+
+        @media (min-width: $bp-sm) {
+            display: none;
         }
     }
 }
