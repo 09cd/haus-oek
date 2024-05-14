fa046e9fd0384b85a42e1e3aca9f822ee2e7678b
Chen Yang Diep <diepchenyang@icloud.com>
2024-05-14 12:08:57 +0200

    Cleanup Navbar

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -2,13 +2,10 @@
 import { gsap } from "gsap";
 
 const colorMode = useColorMode();
+
+const isDarkMode = ref(false);
+
 colorMode.preference = "light";
-
-const menuButton = ref(null);
-const closeButton = ref(null);
-
-// FIX: When the user changes the  color mode of the device, isDarkMode is not updated
-const isDarkMode = ref(colorMode.preference === "dark");
 
 function changeColorMode() {
     timeline.reverse();
@@ -25,10 +22,10 @@ function changeColorMode() {
 const timeline = gsap.timeline({ paused: true });
 
 onMounted(() => {
-    gsap.set(".menu-item p a", { y: 20 });
+    const menuButton = document.querySelector(".navbar__menu-btn");
+    const closeButton = document.querySelector(".overlay__close-btn");
 
-    menuButton.value = document.querySelector(".navbar-menu");
-    closeButton.value = document.querySelector(".close-button");
+    gsap.set(".menu-item a", { y: 20 });
 
     timeline.set(".overlay", { display: "block" });
 
@@ -50,7 +47,7 @@ onMounted(() => {
     );
 
     timeline.to(
-        ".menu-item p a",
+        ".menu-item a",
         {
             duration: 0.7,
             y: 0,
@@ -60,14 +57,14 @@ onMounted(() => {
         "<"
     );
 
-    menuButton.value.addEventListener("click", function () {
+    menuButton.addEventListener("click", function () {
         document.querySelector(".color-mode-btn").textContent = isDarkMode.value
             ? "Light Mode"
             : "Dark Mode";
         timeline.play();
     });
 
-    closeButton.value.addEventListener("click", function () {
+    closeButton.addEventListener("click", function () {
         timeline.reverse();
     });
 });
@@ -75,25 +72,25 @@ onMounted(() => {
 
 <template>
     <nav class="navbar">
-        <ul class="navbar-container">
+        <ul class="navbar__links">
             <li>
-                <NuxtLink to="/about-me" class="nav-link">About Me</NuxtLink>
+                <NuxtLink to="/about-me" class="link">About Me</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/contact" class="nav-link">Contact</NuxtLink>
+                <NuxtLink to="/contact" class="link">Contact</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/blog" class="nav-link">Blog</NuxtLink>
+                <NuxtLink to="/blog" class="link">Blog</NuxtLink>
             </li>
         </ul>
-        <div class="navbar-logo">
-            <NuxtLink to="/" class="nav-link">Haus Oek</NuxtLink>
+        <div class="navbar__logo">
+            <NuxtLink to="/" class="link">Haus Oek</NuxtLink>
         </div>
-        <div class="navbar-menu">Menu</div>
+        <div class="navbar__menu-btn">Menu</div>
     </nav>
     <div class="overlay">
-        <div class="close-button">Close</div>
-        <div class="overlay-menu">
+        <div class="overlay__close-btn">Close</div>
+        <div class="overlay__menu">
             <div class="divider"></div>
             <div class="menu-item">
                 <p>
@@ -144,28 +141,26 @@ onMounted(() => {
     display: flex;
     justify-content: space-between;
     gap: 2rem;
-    padding: 1rem 2rem 1rem 2rem;
     position: sticky;
     top: 0;
     left: 0;
-    // background-color: $white;
+    padding: 1rem 2rem;
     background-color: var(--primary);
-    // border-bottom: 1px solid $black;
     border-bottom: 1px solid var(--secondary);
-    z-index: 2;
+    z-index: 2; // !!!
 
-    .navbar-container {
+    &__links {
         display: none;
+        list-style: none;
+
         @media (min-width: $bp-lg) {
             display: flex;
             justify-content: space-between;
             gap: 2rem;
-
-            list-style: none;
         }
     }
 
-    .navbar-logo {
+    &__logo {
         @media (min-width: $bp-lg) {
             position: absolute;
             left: $pad-left-lg;
@@ -177,14 +172,13 @@ onMounted(() => {
         }
     }
 
-    .nav-link {
-        // color: $black;
-        color: var(--secondary);
-        text-decoration: none;
+    &__menu-btn {
+        cursor: pointer;
     }
 
-    .navbar-menu {
-        cursor: pointer;
+    .link {
+        color: var(--secondary);
+        text-decoration: none;
     }
 }
 
@@ -195,63 +189,52 @@ onMounted(() => {
     left: 0;
     width: 100%;
     height: 100%;
-    background: #111314; // $black
-    // background-color: rgb(4, 4, 34);
-    background-color: black;
-    clip-path: polygon(0 0, 100% 0, 100% 0, 0 0); /* full width, 0 height */
+    background-color: #000;
+    clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);
     will-change: transform;
     z-index: 3;
-    // overflow: scroll;
 
-    .close-button {
+    &__close-btn {
         position: absolute;
         top: 1rem;
         right: 2rem;
+        color: $white;
         cursor: pointer;
-        color: #fffefe; // $white
     }
 
-    .overlay-menu {
-        // position: fixed;
-        // bottom: 5.5rem;
-        // left: 0;
-        height: 100%;
-        width: 100%;
+    &__menu {
         display: grid;
         grid-template-columns: 1fr;
-        // grid-template-rows: repeat(10, 1fr);
-        // grid-template-rows: auto 1fr auto 1fr auto 1fr auto 1fr auto 1fr;
-        grid-template-rows: auto 1fr auto 1fr auto 1fr auto 1fr auto 1fr;
-        overflow: scroll;
+        grid-template-rows: repeat(5, auto 1fr);
+        height: 100%;
+        width: 100%;
+        padding: 3rem 0;
+        gap: 0.5rem;
 
-        // flex-direction: column;
-        // justify-content: end;
-        // padding-bottom: 6rem;
-        padding: 12rem 0 2rem;
-        gap: 1rem;
+        @media (min-height: $bp-sm) {
+            padding: 12rem 0 2rem;
+            gap: 1rem;
+        }
 
         .divider {
             width: 100%;
             height: 1px;
-            background: #fffefe; // $white
-            // margin: 5.5rem 0 2rem;
-            // margin: 5% 0 2%;
+            background: $white;
         }
 
         .menu-item {
-            // padding: 2rem 0 5.5rem;
             cursor: pointer;
 
             p {
                 clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
-                // clip-path: inset(0);
                 overflow: hidden;
 
                 a {
                     padding-left: 2rem;
-                    color: #fffefe; // $white
+                    color: $white;
                     text-decoration: none;
                     display: inline-block;
+
                     @media (min-width: $bp-sm) {
                         padding-left: 8rem;
                     }
