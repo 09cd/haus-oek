947565f3cc3084c6635682d7d8f776aa570a5785
Chen Yang Diep <diepchenyang@icloud.com>
2024-05-14 23:30:41 +0200

    Moved svg to assets folder

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -70,22 +70,7 @@ onMounted(() => {
 
 <template>
     <div class="back-link">
-        <svg
-            fill="grey"
-            height="12px"
-            width="12px"
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            viewBox="0 0 476.213 476.213"
-            xml:space="preserve"
-        >
-            <polygon
-                points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 
-	                57.427,253.107 476.213,253.107 "
-            />
-        </svg>
+        <img src="/assets/images/left-arrow.svg" alt="left-arrow" />
         <NuxtLink to="/blog" class="all-blogs-link">All Blogs</NuxtLink>
     </div>
     <div class="blog-container">
