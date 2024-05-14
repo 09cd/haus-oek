18ec8aa70c1431f3ea191311cb21450e7c48d4c8
Chen Yang Diep <diepchenyang@icloud.com>
2024-05-14 08:53:51 +0200

    Back Link to All Blogs

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -62,6 +62,25 @@ onMounted(() => {
 </script>
 
 <template>
+    <div class="back-link">
+        <svg
+            fill="grey"
+            height="12px"
+            width="12px"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 476.213 476.213"
+            xml:space="preserve"
+        >
+            <polygon
+                points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 
+	                57.427,253.107 476.213,253.107 "
+            />
+        </svg>
+        <NuxtLink to="/blog" class="all-blogs-link">All Blogs</NuxtLink>
+    </div>
     <div class="blog-article">
         <main>
             <nav>
@@ -83,14 +102,73 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
+.back-link {
+    display: flex;
+    padding-left: 2rem;
+    padding-top: 2rem;
+    align-items: center;
+
+    @media (min-width: $bp-lg) {
+        padding-left: $pad-left-lg;
+    }
+
+    @media (min-width: $bp-xl) {
+        padding-left: $pad-left-xl;
+    }
+}
+
+.all-blogs-link {
+    // padding: 2rem 2rem 0;
+    // color: var(--secondary);
+    color: grey;
+    text-decoration: none;
+    font-size: 0.8rem;
+    overflow: hidden;
+
+    padding-left: 0.75rem;
+
+    display: block;
+    position: relative;
+
+    // transition: padding 0.3s;
+
+    // &:hover {
+    //     padding-left: 1.5rem;
+    // }
+
+    // @media (min-width: $bp-lg) {
+    //     padding-left: $pad-left-lg;
+    // }
+
+    // @media (min-width: $bp-xl) {
+    //     padding-left: $pad-left-xl;
+    // }
+
+    // &::before {
+    //     content: url("/assets/images/left-arrow.svg");
+    //     padding-right: 0.5rem;
+    //     // display: none;
+    //     position: relative;
+    //     // clip-path: polygon(100% 100%, 100% 100%, 100% 0, 100% 0);
+    //     transition: padding 0.3s;
+    //     fill: red;
+    // }
+
+    // &:hover::before {
+    //     padding-right: 1rem;
+    //     padding-left: 0.5rem;
+    //     // clip-path: polygon(0 100%, 100% 100%, 100% 0, 0 0);
+    //     // display: inline;
+    // }
+}
 .blog-article {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    min-height: calc(100vh - 3.5rem);
+    min-height: calc(100vh - 6.5rem);
 }
 main {
-    padding: 4rem 2rem 0rem;
+    padding: 2rem 2rem 0rem;
     display: flex;
     flex-direction: column;
     gap: 4rem;
