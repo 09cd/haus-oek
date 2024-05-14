692292ce8e98b92ab26d44b0471bcd9d4cbc0286
Chen Yang Diep <diepchenyang@icloud.com>
2024-05-14 12:08:18 +0200

    Cleanup Footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,31 +1,23 @@
-<script></script>
-
 <template>
     <footer class="footer">
         <div>Imprint, Privacy Policy</div>
-        <div class="copyright">Copyright © 2024 Code With Sway</div>
-        <!-- <div class="copyright-short">© 2024 Code With Sway</div> -->
+        <div class="footer__copyright">Copyright © 2024 Haus Oek</div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
 .footer {
     display: flex;
-    justify-content: space-between;
     flex-direction: column;
-    // padding: 0 2rem 2rem;
-    padding: 2rem;
-    // position: absolute;
-    // bottom: 0;
-    // left: 0;
+    justify-content: space-between;
     width: 100%;
+    padding: 2rem;
 
     @media (min-width: $bp-sm) {
         flex-direction: row;
     }
 
-    .copyright {
-        position: static;
+    &__copyright {
         display: none;
 
         @media (min-width: $bp-sm) {
@@ -39,14 +31,6 @@
 
         @media (min-width: $bp-xl) {
             left: $pad-left-xl;
-        }
-    }
-
-    .copyright-short {
-        display: block;
-
-        @media (min-width: $bp-sm) {
-            display: none;
         }
     }
 }
